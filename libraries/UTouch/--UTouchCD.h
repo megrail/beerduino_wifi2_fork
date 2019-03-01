71c4e04a1f8690fa798334b09d8abb9cc986ad34
Pavel Rusin <p_rusin@wargaming.net>
2019-03-01 14:23:14 +0300

    initial state

---
+++ libraries/UTouch/--UTouchCD.h
@@ -0,0 +1,36 @@
+// UTouchCD.h
+// ----------
+//
+// Since there are slight deviations in all touch screens you should run a
+// calibration on your display module. Run the UTouch_Calibration sketch
+// that came with this library and follow the on-screen instructions to
+// update this file.
+//
+// Remember that is you have multiple display modules they will probably 
+// require different calibration data so you should run the calibration
+// every time you switch to another module.
+// You can, of course, store calibration data for all your modules here
+// and comment out the ones you dont need at the moment.
+//
+
+// These calibration settings works with my ITDB02-3.2S.
+// They MIGHT work on your display module, but you should run the
+// calibration sketch anyway.
+//#define CAL_X 0x03C7812AUL
+//#define CAL_Y 0x03DEC1DDUL
+//#define CAL_S 0x000EF13FUL
+//#define CAL_X 0x03AF00EEUL 
+//#define CAL_Y 0x03BFC178UL 
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03C300D0UL
+//#define CAL_Y 0x03CD016FUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x00384ED3UL //опнейр
+//#define CAL_Y 0x03BB8178UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03CC80D0UL
+//#define CAL_Y 0x03D7C184UL
+//#define CAL_S 0x000EF13FUL
---
+++ libraries/UTouch/UTouchCD.h
@@ -0,0 +1,106 @@
+// UTouchCD.h
+// ----------
+//
+// Since there are slight deviations in all touch screens you should run a
+// calibration on your display module. Run the UTouch_Calibration sketch
+// that came with this library and follow the on-screen instructions to
+// update this file.
+//
+// Remember that is you have multiple display modules they will probably 
+// require different calibration data so you should run the calibration
+// every time you switch to another module.
+// You can, of course, store calibration data for all your modules here
+// and comment out the ones you dont need at the moment.
+//
+
+// These calibration settings works with my ITDB02-3.2S.
+// They MIGHT work on your display module, but you should run the
+// calibration sketch anyway.
+
+//#define CAL_X 0x003B0F01UL
+//#define CAL_Y 0x03C74182UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03AB00D5UL
+//#define CAL_Y 0x03D401AEUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03A380FCUL
+//#define CAL_Y 0x03D4019EUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x01FF8A55UL
+//#define CAL_Y 0x034A81ECUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03C080EDUL
+//#define CAL_Y 0X03D4019BUL
+//#define CAL_S 0X000EF13FUL
+
+//#define CAL_X 0x03C1C126UL
+//#define CAL_Y 0x03D3C1A9UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03B880D5UL
+//#define CAL_Y 0x03C6C181UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03CA80C5UL
+//#define CAL_Y 0x03CBC184UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03BA40C6UL
+//#define CAL_Y 0x03C0016DUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03BE8123UL 
+//#define CAL_Y 0x03DD41A3UL 
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03AE80E3UL
+//#define CAL_Y 0x03BD41A9UL
+//#define CAL_S 0x000EF18FUL
+
+//#define CAL_X 0x03C7C10EUL 
+//#define CAL_Y 0x03D8019DUL 
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x02F5850CUL
+//#define CAL_Y 0x02F6C4EBUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x00384ED3UL //ПРОЕКТ
+//#define CAL_Y 0x03BB8178UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x003B0E9EUL
+//#define CAL_Y 0x03BE416CUL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x003E8E3DUL 
+//#define CAL_Y 0x03AAC188UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x00000000UL 
+//#define CAL_Y 0x00000000UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x00D20E2FUL 
+//#define CAL_Y 0x03BB8172UL 
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03BF80E1UL
+//#define CAL_Y 0x03DD81A6UL
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03C9C0FCUL 
+//#define CAL_Y 0x03DE81AEUL 
+//#define CAL_S 0x000EF13FUL
+
+//#define CAL_X 0x03D142F5UL
+//#define CAL_Y 0x03D90196UL
+//#define CAL_S 0x000EF13FUL
+
+#define CAL_X 0x03AB8093UL 
+#define CAL_Y 0x039FC151UL 
+#define CAL_S 0x8013F0EFUL
